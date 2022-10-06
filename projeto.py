7f719efa7afef2087cbfe9a9017b8970ffe3d121
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2022-10-06 16:33:15 -0300

    Create projeto.py
    primeiro commit

---
+++ projeto.py
@@ -0,0 +1,39 @@
+
+## projeto com conversão de temperaturas, calculo de acelaração, energia e etc
+
+
+def f_to_c (f_temp):
+    c_temp = (f_temp - 32) * 5/9
+    return c_temp 
+
+f100_in_celsius = f_to_c(100)
+
+def c_to_f (c_temp):
+    f_temp = (c_temp * (9/5)) + 32
+    return f_temp
+
+c0_in_fahrenheit = c_to_f(0)
+
+def get_force(mass, acceleration):
+    return mass * acceleration
+
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+train_force = get_force(train_mass, train_acceleration)
+print("The GE train supplies " + str(train_force) +" Newtons of force.")
+
+def get_energy(mass, c = 3*(10**8)):
+    mass *= c ** 2
+    return mass
+
+bomb_mass = 1
+bomb_energy = get_energy(bomb_mass)
+print(bomb_energy)
+
+def get_work(mass, acceleration, distance):
+    force = get_force(mass, acceleration) * train_distance
+    return force
+
+train_work = get_work(train_mass, train_acceleration, train_distance)
+print("The GE train does "+ str(train_work) + " Joules of work over " + str(train_distance) + " meters")
